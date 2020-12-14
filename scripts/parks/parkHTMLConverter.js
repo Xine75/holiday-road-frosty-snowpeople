26b7d667df73d6373b7f6ce4bc35d76026c1b25e
Ni236630 <nicholas.douso@gmail.com>
2020-12-14 14:49:39 -0600

    finished selector; working on html converter

--- scripts/parks/parkHTMLConverter.js
+++ scripts/parks/parkHTMLConverter.js
@@ -1,32 +1,18 @@
 // Translate JS api data into HTML(Will be sent to the List component)
 // Also handles the selectors from the option dropdowns
 
-import { useParks, getParks } from "./ParkProvider.js"
 
 
- const parkSelectorTarget = document.querySelector(".navbar__park")
-
-
-// export const parkHTMLConverter = () => {
-//   return `
-  
-  
-  
-//   `
-// }
-
-
-export const parkSelector = () => {
-  getParks().then(()=>
-    parkSelectorTarget.innerHTML = `
-    <select class="dropdown" id="parkSelect">
-      <option value="0">Please select a park...</option>
-      ${
-        useParks().map(parkObject =>{
-          const allParks = parkObject.fullName 
-          return `<option>${allParks}</options>`
-        })
-      }
-    </select>`
-  )
+export const parkHTMLConverter = () => {
+  return `
+    <section class="park__data">
+     <div>
+      <h2>${parkObject.fullName} </h2>
+      <p>Website:${parkObject.url}</p>
+      <p></p>   
+      <button class="detail__parks">Details</button>
+     </div>
+  </section>
+  `
 }
+
