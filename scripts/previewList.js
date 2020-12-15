24bcfd712fd31cee83be62f7169121dcc19ab78c
Ni236630 <nicholas.douso@gmail.com>
2020-12-15 10:08:31 -0600

    added import to previewList.js

--- scripts/previewList.js
+++ scripts/previewList.js
@@ -1,9 +1,4 @@
-import { parkHTMLConverter } from "./parks/parkHTMLConverter.js";
-import { getParks, useParks } from "./parks/ParkProvider.js"
-import { parkSelector } from "./parks/parkSelector.js"
 
-getParks()
-parkSelector()
 /* 
     ------IMPORTS------
     All HTML converter components (except weather) - These also include selector components
@@ -11,18 +6,4 @@ parkSelector()
     All dialogue components (previewList will listen for detail button clicked)
     
 */
-
-
-/* 
-Rendering parks to the DOM
-*/
-const parkPreviewTarget = document.querySelector(".preview__park")
-
-const renderPark = () => {
-    getParks().then(() => 
-     parkPreviewTarget.innerHTML = `${useParks().map((parkObject) => parkHTMLConverter(parkObject)).join("")}
-    `
-   )
-    return parkPreviewTarget
-}; 
-  renderPark()
+ import "./parks/parkPreview.js"
