dd96f96142fbf721f24385c34fc59f5225d043df
Ni236630 <nicholas.douso@gmail.com>
2020-12-16 14:19:19 -0600

    finalized dialog render; working on details

--- scripts/parks/parkDialogue.js
+++ scripts/parks/parkDialogue.js
@@ -1,2 +1,43 @@
 // previewList.js listens for a details button to be pressed and is redirected here 
 // Presents additional park information to the user via dialogue box
+
+import { useParks } from "./ParkProvider.js";
+
+// hub for listening for details button
+const eventHub = document.querySelector(".container")
+
+
+eventHub.addEventListener("click", event =>{
+  if (event.target.id === "closeDialog"){
+    parkDialog.close()
+  }
+})
+
+eventHub.addEventListener("parkDetailClicked", customEvent =>{
+  const parkDialog = document.querySelector("#parkDialog")
+  const parkDialogText = document.querySelector("#parkDialog__text")
+  
+
+  const clickedPark = useParks().find( park  => park.id === customEvent.detail.clickedParkId)
+
+  parkDialogText.innerHTML = `
+    <h3>${clickedPark.name}</h3>
+    <p>Entrance Fees:$${clickedPark.entranceFees[0].cost}</p>
+    <p>Directions: ${clickedPark.directionsInfo}</p>
+    
+  `
+parkDialog.showModal()
+  
+})
+
+
+// export this to be planted in parkPreview render or converter?
+export const parkDialog = () => {
+  return `
+    <dialog id="parkDialog">
+      <div id="parkDialog__text"></div>
+      <button id="closeDialog__park">close</button>
+    </dialog>
+  `
+  
+}
