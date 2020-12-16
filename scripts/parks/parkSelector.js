209143bb20aa4f1591ed729df8badf50a6932f9a
Ni236630 <nicholas.douso@gmail.com>
2020-12-16 08:19:50 -0600

    fixed loop bug in selector

--- scripts/parks/parkSelector.js
+++ scripts/parks/parkSelector.js
@@ -1,50 +1,47 @@
-import { useParks, getParks } from "./ParkProvider.js"
+import { useParks, getParks } from "./ParkProvider.js";
 
-const parkSelectorTarget = document.querySelector(".navbar__park")
+const parkSelectorTarget = document.querySelector(".navbar__park");
 
-const eventHub = document.querySelector(".container")
+const eventHub = document.querySelector(".container");
 
 export const parkSelector = () => {
-  getParks().then(()=>
-    parkSelectorTarget.innerHTML = `
+  getParks().then(
+    () =>
+      (parkSelectorTarget.innerHTML = `
     <select class="dropdown" id="parkSelect">
       <option value="0">Please select a park...</option>
-      ${
-        useParks().map(parkObject =>{
-          const allParks = parkObject.fullName 
-          return `<option value=${parkObject.id}>${allParks}</options>`
+      ${useParks()
+        .map((parkObject) => {
+          const allParks = parkObject.fullName;
+          return `<option value=${parkObject.id}>${allParks}</options>`;
         })
+        .join("")}
+    </select>`)
+  );
+};
+
+//event to dispatch selection SWITCH TO FIND METHOD FOR CONCISE CODE
+eventHub.addEventListener("change", (changeEvent) => {
+  if (changeEvent.target.id === "parkSelect") {
+    const parks = useParks();
+    let parkId = "0";
+    let longitude = 0;
+    let latitude = 0;
+    for (const park of parks) {
+      if (changeEvent.target.value === park.id) {
+        parkId = park.id;
+        longitude = park.longitude;
+        latitude = park.latitude;
       }
-    </select>`
-  )
-}
-
-//event to dispatch selection
-eventHub.addEventListener("change", changeEvent => {
-
-  if (changeEvent.target.id === "parkSelect" ){
-    const parks = useParks()
-    let parkId = "0"
-    let longitude = 0
-    let latitude = 0
-    for (const park of parks){
-      if(changeEvent.target.value === park.id){
-      parkId = park.id
-      longitude = park.longitude
-      latitude = park.latitude
-        
-
       }
+      const customEvent = new CustomEvent("parkSelected", {
+        detail: {
+          parkID: parkId,
+          latitude: latitude,
+          longitude: longitude,
+        },
+      });
+      eventHub.dispatchEvent(customEvent);
     
-
-    const customEvent = new CustomEvent("parkSelected",{
-      detail: {
-        parkID: parkId,
-        latitude: latitude ,
-        longitude: longitude
-        
-      }
-
-    })
   }
-})
+});
