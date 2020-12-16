679936d26e15d9dfee3a8485b1314ee759249c64
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2020-12-16 13:46:08 -0600

    Prepare for merge

--- scripts/savedItineraries/itineraryForm.js
+++ scripts/savedItineraries/itineraryForm.js
@@ -1,7 +1,7 @@
 // Handles the user-input itinerary name & save itinerary button in the previewList
 
 // Selectors
-
+const eventHub = document.querySelector(".container");
 
 // These variables will hold the last related selected option 
 // e.g. parkId will hold the ID of the last park object selected
@@ -9,3 +9,15 @@ let parkId = "";
 let attractionId = "";
 let eateryId = "";
 
+// 
+eventHub.addEventListener("parkSelected", (event) => {
+
+})
+
+eventHub.addEventListener("parkSelected", (event) => {
+
+})
+
+eventHub.addEventListener("parkSelected", (event) => {
+
+})
